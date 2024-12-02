c5dccff4d6da1ec6f84b208a889bedef8b1eabeb
rupaak <rupakt525@gmail.com>
2024-12-02 09:49:31 +0545

    deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,14 +2,13 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.resolve.alias = {
-          ...config.resolve.alias,
-          '@maily-to/core': path.resolve(__dirname, '../../packages/core/dist'),
-        };
-        return config;
-      },
-    
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@maily-to/core': path.resolve(__dirname, '../../packages/core/dist'),
+    };
+    return config;
+  }
 };
 
 module.exports = nextConfig;
