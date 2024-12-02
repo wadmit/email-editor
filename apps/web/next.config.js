cedb2ea7c85c7840c571e273f4d204e8211fa44e
rupaak <rupakt525@gmail.com>
2024-12-02 15:27:08 +0545

    deploy

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
