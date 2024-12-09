c721b8e91a3cf4502b652ddc541925721f748f0f
rupaak <rupakt525@gmail.com>
2024-12-09 17:18:51 +0545

    deploy

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,37 +1,38 @@
-// import { NextResponse, type NextRequest } from 'next/server';
-// import { updateSession } from './lib/supabase/middleware';
-// import axios from 'axios';
+import { NextRequest, NextResponse } from 'next/server';
+import { updateSession } from './lib/supabase/middleware';
+import axios from 'axios';
 
-// export async function middleware(req) {
-//   const token = req.cookies.get('accessToken') || null; 
-//   const url = req.nextUrl.clone();
+export async function middleware(req) {
+  const token = req.cookies.get('accessToken') || null; 
+  const url = req.nextUrl.clone();
 
-//   if (!token) {
-//     url.pathname = '/login';
-//     return NextResponse.redirect(url);
-//   }
+  NextResponse.next()
+  // if (!token) {
+  //   url.pathname = '/login';
+  //   return NextResponse.redirect(url);
+  // }
 
-//   try {
-//     const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/dashboard/auth/profile`, {
-//       method: 'GET',
-//       headers: {
-//         Authorization: `Bearer ${token.value}`,
-//       },
-//     });
+  // try {
+  //   const response = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/dashboard/auth/profile`, {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: `Bearer ${token.value}`,
+  //     },
+  //   });
 
-//     if (!response.data.data) {
-//       url.pathname = '/login'; 
-//       return NextResponse.redirect(url);
-//     }
-//   } catch (error) {
-//     url.pathname = '/login'; // Redirect on error
-//     return NextResponse.redirect(url);
-//   }
+  //   if (!response.data.data) {
+  //     url.pathname = '/login'; 
+  //     return NextResponse.redirect(url);
+  //   }
+  // } catch (error) {
+  //   url.pathname = '/login'; // Redirect on error
+  //   return NextResponse.redirect(url);
+  // }
 
-//   // Continue to the requested route if token is valid
-//   return NextResponse.next();
-// }
+  // // Continue to the requested route if token is valid
+  // return NextResponse.next();
+}
 
-// export const config = {
-//   matcher: ['/'], 
-// };
+export const config = {
+  matcher: ['/'], 
+};
